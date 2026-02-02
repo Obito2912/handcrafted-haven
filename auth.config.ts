abe7308ec938bed20ea511d6f8f2b4d0a83067d8
Ovinson Lugo <ovinsonlugo12@gmail.com>
2026-02-01 21:00:54 -0700

    Create feature for login page to greet user if signed in

--- auth.config.ts
+++ auth.config.ts
@@ -1,21 +1,33 @@
-import type { NextAuthConfig } from 'next-auth';
- 
+import type { NextAuthConfig } from "next-auth";
+
 export const authConfig = {
   pages: {
-    signIn: '/login',
+    signIn: "/login",
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
       if (isOnDashboard) {
         return true;
-      //   if (isLoggedIn) return true;
-      //   return false; // Redirect unauthenticated users to login page
-      // } else if (isLoggedIn) {
-      //   return Response.redirect(new URL('/dashboard', nextUrl));
+        //   if (isLoggedIn) return true;
+        //   return false; // Redirect unauthenticated users to login page
+        // } else if (isLoggedIn) {
+        //   return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token?.id) {
+        session.user.id = token.id as string;
+      }
+      return session;
     },
   },
   providers: [], // Add providers with an empty array for now
--- components/Aside/Aside.tsx
+++ components/Aside/Aside.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import './Aside.css';
 import Navigation from "../Navigation/Navigation";
+import { signOut } from 'next-auth/react';
+import { usePathname } from 'next/navigation';
 
 export default function Aside() {
+    const pathname = usePathname();
+
     return <aside className="sidebar">
         <Navigation />
-        <button className='sign-out'>Sign Out</button>
+        <button className='sign-out' onClick={() => signOut({ callbackUrl: pathname })}>Sign Out</button>
     </aside>;
 }
