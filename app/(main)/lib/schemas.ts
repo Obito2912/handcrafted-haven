e824005cb22bb6f73dda6f8f61eaa4172a846435
Ovinson Lugo <ovinsonlugo12@gmail.com>
2026-02-01 17:14:04 -0700

    Update color pallete for main page. Add settings and profile pic on header. Update backend to also take a url for the profile picture.

--- app/(main)/lib/schemas.ts
+++ app/(main)/lib/schemas.ts
@@ -1,45 +1,54 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 const FormSchema = z.object({
-  name: z.string()
-    .min(2, 'Name must be at least 2 characters')
-    .max(100),
+  name: z.string().min(2, "Name must be at least 2 characters").max(100),
+  profile_image_url: z
+    .url("Profile Image URL must be a valid URL")
+    .max(200, "Profile Image URL must be at most 200 characters"),
   email: z.email(),
-  password: z.string()
-  .min(6, 'Password must be at least 6 characters')
-  .max(100)
-//   .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
-//   .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
-//   .regex(/[0-9]/, 'Password must contain at least one number')
-//   .regex(/[^A-Za-z0-9]/, 'Password must contain at least one special character'),  
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .max(100),
+  //   .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+  //   .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+  //   .regex(/[0-9]/, 'Password must contain at least one number')
+  //   .regex(/[^A-Za-z0-9]/, 'Password must contain at least one special character'),
 });
 
-export const LoginFormSchema = FormSchema.omit({ name: true});
+export const LoginFormSchema = FormSchema.omit({
+  name: true,
+  profile_image_url: true,
+});
 export const SignupFormSchema = FormSchema;
 type FormState<TFields extends string> = {
-    errors?: Partial<Record<TFields, string[]>>;
-    message?: string | null;
-    values?: {
+  errors?: Partial<Record<TFields, string[]>>;
+  message?: string | null;
+  values?: {
     name?: string;
+    profile_image_url?: string;
     email?: string;
-}
+  };
 };
 
-export type AuthFormState = FormState<'name' | 'email' | 'password'>;
-export type ProfileFormState = FormState<'user_id' | 'name' | 'bio' | 'gender' | 'image_url' | 'age'>;
+export type AuthFormState = FormState<
+  "name" | "profile_image_url" | "email" | "password"
+>;
+export type ProfileFormState = FormState<
+  "user_id" | "name" | "bio" | "gender" | "profile_image_url" | "age"
+>;
 export const ProfileSchema = z.object({
-    user_id: z.uuid(),
-    name: z.string()
-      .min(2, 'Name must be at least 2 characters')
-      .max(100),
-    bio: z.string()
-      .max(500, 'Bio must be at most 500 characters'),
-    gender: z.enum(['Male', 'Female']),
-    image_url: z.string()
+  user_id: z.uuid(),
+  name: z.string().min(2, "Name must be at least 2 characters").max(100),
+  bio: z.string().max(500, "Bio must be at most 500 characters"),
+  gender: z.enum(["Male", "Female"]),
+  profile_image_url: z
+    .string()
     //.url('Image URL must be a valid URL')
-    .max(200, 'Image URL must be at most 200 characters'),
-    age: z.number()
-      .min(10, 'Age must be at least 10')
-      .max(120, 'Age must be at most 120')
-      .nullable(),
+    .max(200, "Image URL must be at most 200 characters"),
+  age: z
+    .number()
+    .min(10, "Age must be at least 10")
+    .max(120, "Age must be at most 120")
+    .nullable(),
 });
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,12 +1,19 @@
 import './Header.css';
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <header className="header">
-      <div>
+      <div className='header__logo-container'>
         <i className="fa-regular fa-house"></i>
         <span className='header__title'>HandCrafted Haven</span>
         <i className="fa-solid fa-leaf"></i>
+      </div>
+      <div className='header__settings-container'>
+        <button className='header__settings-button'>
+          <a href="/settings">Settings</a>
+        </button>
+        <Image className='header__profile-image' src="" alt="Profile Image" width={24} height={24} />
       </div>
     </header>
   );
