712feef72d68a97f5239bb97959c2c51dcbac480
Ovinson Lugo <ovinsonlugo12@gmail.com>
2026-02-16 21:09:34 -0700

    Adjust styles for artisans page by creating rounded background on top

--- components/main/Artisans/ArtisansDisplay.tsx
+++ components/main/Artisans/ArtisansDisplay.tsx
@@ -14,11 +14,11 @@ export default function ArtisansDisplay({ userProfile, user }: ArtisansDisplayPr
       {userProfile ? (
         <article className={styles.artisanProfile} aria-label="Artisans Profile">
           {userProfile.image_url && <Image src={userProfile.image_url ?? "/users/amy-burns.png"} alt={`profile picture of ${userProfile.name ?? "Artisan"}`} width={100} height={100} />}
-          <h2>{userProfile.name ?? user?.email}</h2>
-          {userProfile.bio && <p>{userProfile.bio}</p>}
-          {userProfile.age && <p>Age: {userProfile.age}</p>}
-          {userProfile.gender && <p>Gender: {userProfile.gender === "female" ? "Female" : "Male"}</p>}
-          <p>Joined: {user ? new Date(user.created_at).toLocaleDateString() : "Unknown"}</p>
+          <h2 className={styles.artisanProfile__heading}>{userProfile.name ?? user?.email}</h2>
+          {userProfile.bio && <p className={styles.artisanProfile__bio}>{userProfile.bio}</p>}
+          {userProfile.age && <p className={styles.artisanProfile__age}>Age: {userProfile.age}</p>}
+          {userProfile.gender && <p className={styles.artisanProfile__gender}>Gender: {userProfile.gender === "female" ? "Female" : "Male"}</p>}
+          <p className={styles.artisanProfile__joined}>Joined: {user ? new Date(user.created_at).toLocaleDateString() : "Unknown"}</p>
         </article>
       ) : (
         <p role="status">No profile found.</p>
